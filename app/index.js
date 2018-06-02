9fad7f0c0a7af695b98146ec295678abbef1f8ad
isaaclem <lem.chiew.choon@gmail.com>
2018-06-02 12:26:16 +0800

    completed options screen

--- app/index.js
+++ app/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 //import Home from './screens/Home';
-import CurrencyList from './screens/CurrencyList';
+//import CurrencyList from './screens/CurrencyList';
+//import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -13,4 +15,4 @@ EStyleSheet.build({
   $darkText: '#343434',
   //$outline: 1
 });
-export default () => <CurrencyList />;
+export default () => <Themes />;
